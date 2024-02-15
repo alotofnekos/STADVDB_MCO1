95c0ec35dc758e66264e97053d72a396070992c7
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2024-02-15 16:47:57 +0800

    Update createtables_MCO1.sql

--- sql/createtables_MCO1.sql
+++ sql/createtables_MCO1.sql
@@ -1,14 +1,14 @@
 CREATE TABLE px (
     pxid VARCHAR(32) NOT NULL,
-    age INT DEFAULT -1 CHECK (age >= 0 OR age IS NULL) NOT NULL,
-    gender ENUM('MALE', 'FEMALE', 'N/A', 'OTHER') DEFAULT 'N/A' NOT NULL,
+    age INT DEFAULT -1,
+    gender ENUM('MALE', 'FEMALE', 'NOTAVAILABLE', 'OTHER') DEFAULT 'NOTAVAILABLE',
     PRIMARY KEY (pxid)
 );
 
 CREATE TABLE clinics (
     clinicid VARCHAR(32) NOT NULL,
     hospitalname VARCHAR(255) NOT NULL,
-    IsHospital ENUM('TRUE', 'FALSE', 'N/A') DEFAULT 'N/A' NOT NULL,
+    IsHospital ENUM('TRUE', 'FALSE', 'NOTAVAILABLE') DEFAULT 'NOTAVAILABLE' NOT NULL,
     City VARCHAR(50) NOT NULL,
     Province VARCHAR(50) NOT NULL,
     RegionName VARCHAR(255) NOT NULL,
@@ -17,8 +17,8 @@ CREATE TABLE clinics (
 
 CREATE TABLE doctors (
     doctorid VARCHAR(32) NOT NULL,
-    mainspecialty VARCHAR(255) DEFAULT 'N/A' NOT NULL,
-    age INT DEFAULT -1 CHECK (age >= 0 OR age IS NULL) NOT NULL,
+    mainspecialty VARCHAR(255) DEFAULT 'NOTAVAILABLE',
+    age INT DEFAULT -1,
     PRIMARY KEY (doctorid)
 );
 
@@ -33,7 +33,7 @@ CREATE TABLE appointments (
     StartTime DATETIME,
     EndTime DATETIME,
     app_type VARCHAR(20),
-    is_Virtual ENUM('TRUE', 'FALSE', 'N/A') DEFAULT 'N/A' NOT NULL,
+    is_Virtual ENUM('TRUE', 'FALSE', 'NOTAVAILABLE') DEFAULT 'NOTAVAILABLE' NOT NULL,
     PRIMARY KEY (apptid),
     FOREIGN KEY (pxid) REFERENCES px(pxid),
     FOREIGN KEY (clinicid) REFERENCES clinics(clinicid),
