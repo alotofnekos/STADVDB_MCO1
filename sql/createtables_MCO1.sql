027ee12289776770f305c004ea944a0ec10a7d75
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2024-02-15 21:31:53 +0800

    Update createtables_MCO1.sql

--- sql/createtables_MCO1.sql
+++ sql/createtables_MCO1.sql
@@ -5,13 +5,19 @@ CREATE TABLE px (
     PRIMARY KEY (pxid)
 );
 
+CREATE TABLE px_dup (
+    pxid VARCHAR(32) NOT NULL,
+    age INT DEFAULT -1,
+    gender ENUM('MALE', 'FEMALE', 'NOTAVAILABLE', 'OTHER') DEFAULT 'NOTAVAILABLE'
+);
+
 CREATE TABLE clinics (
     clinicid VARCHAR(32) NOT NULL,
-    hospitalname VARCHAR(255) NOT NULL,
-    IsHospital ENUM('TRUE', 'FALSE', 'NOTAVAILABLE') DEFAULT 'NOTAVAILABLE' NOT NULL,
-    City VARCHAR(50) NOT NULL,
-    Province VARCHAR(50) NOT NULL,
-    RegionName VARCHAR(255) NOT NULL,
+    hospitalname VARCHAR(255),
+    IsHospital ENUM('TRUE', 'FALSE', 'NOTAVAILABLE') DEFAULT 'NOTAVAILABLE',
+    City VARCHAR(50),
+    Province VARCHAR(50),
+    RegionName VARCHAR(255),
     PRIMARY KEY (clinicid)
 );
 
@@ -27,13 +33,13 @@ CREATE TABLE appointments (
     clinicid VARCHAR(32) NOT NULL,
     doctorid VARCHAR(32) NOT NULL,
     apptid VARCHAR(32) NOT NULL,
-    status VARCHAR(20) NOT NULL,
+    status VARCHAR(20),
     TimeQueued DATETIME,
     QueueDate DATETIME,
     StartTime DATETIME,
     EndTime DATETIME,
     app_type VARCHAR(20),
-    is_Virtual ENUM('TRUE', 'FALSE', 'NOTAVAILABLE') DEFAULT 'NOTAVAILABLE' NOT NULL,
+    is_Virtual ENUM('TRUE', 'FALSE', 'NOTAVAILABLE') DEFAULT 'NOTAVAILABLE',
     PRIMARY KEY (apptid),
     FOREIGN KEY (pxid) REFERENCES px(pxid),
     FOREIGN KEY (clinicid) REFERENCES clinics(clinicid),
