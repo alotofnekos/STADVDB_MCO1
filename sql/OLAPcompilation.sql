8f486e65f4636784aefb12999b0af66690d98b76
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2024-02-20 22:15:38 +0800

    Update OLAPcompilation.sql

--- sql/OLAPcompilation.sql
+++ sql/OLAPcompilation.sql
@@ -49,8 +49,38 @@ GROUP BY
     Month,
     status;
 #OLAP2 indexing
-CREATE INDEX idx_appointments_timeQueued ON appointments (TimeQueued);
+CREATE INDEX idx_appointments_OLAP2 ON appointments (TimeQueued, status);
+#dont forget to drop this index before OLAP3
 #OLAP2 Denorm
 #Not applicable
 
-
+#OLAP3
+SELECT
+    CASE MONTH(TimeQueued)
+        WHEN 1 THEN 'January'
+        WHEN 2 THEN 'February'
+        WHEN 3 THEN 'March'
+        WHEN 4 THEN 'April'
+        WHEN 5 THEN 'May'
+        WHEN 6 THEN 'June'
+        WHEN 7 THEN 'July'
+        WHEN 8 THEN 'August'
+        WHEN 9 THEN 'September'
+        WHEN 10 THEN 'October'
+        WHEN 11 THEN 'November'
+        WHEN 12 THEN 'December'
+    END AS Month,
+    is_Virtual,
+    COUNT(*) AS VirtualCount
+FROM
+    appointments
+WHERE
+    YEAR(TimeQueued) = 2021
+GROUP BY
+    Month,
+    is_Virtual;
+#OLAP 3 Index
+CREATE INDEX idx_appointments_OLAP3 ON appointments (TimeQueued, is_Virtual);
+#dont forget to drop this index before OLAP4
+#OLAP 3 Denorm
+# Not Applicable
